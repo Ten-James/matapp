9ca718790c48b363728bfa98df584c9691c31349
Ten-James <hexaplay2@gmail.com>
2022-10-20 12:51:10 +0200

    animated navigation

--- MatAppClient/src/views/admin/navigation/betterLinks.tsx
+++ MatAppClient/src/views/admin/navigation/betterLinks.tsx
@@ -2,64 +2,72 @@ import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { navigationContext } from "./index";
 interface BetterLinkProps {
-  to: string;
-  requiredAdmin?: boolean;
-  expandedText: string;
-  icon: string;
-  onClick?: () => void;
+	to: string;
+	requiredAdmin?: boolean;
+	expandedText: string;
+	icon: string;
+	onClick?: () => void;
 }
 
 interface SubBetterLinkProps {
-  to: string;
-  expandedText: string;
-  requiredAdmin?: boolean;
-  icon: string;
-  children: JSX.Element | JSX.Element[];
+	to: string;
+	expandedText: string;
+	requiredAdmin?: boolean;
+	icon: string;
+	children: JSX.Element | JSX.Element[];
 }
 
+const ExpandedText = ({ isExpanded, text }: { isExpanded: boolean; text: string }) => {
+	return (
+		<div className='text' style={{ paddingLeft: isExpanded ? "5.5em" : "0" }}>
+			<p style={{ transform: isExpanded ? "translateX(-5)" : "translateX(-5em)" }}> {text}</p>
+		</div>
+	);
+};
+
 export const BetterLink = ({ to, expandedText, icon, requiredAdmin, onClick }: BetterLinkProps) => {
-  requiredAdmin = requiredAdmin || false;
-  const { isExpanded, userAccess } = useContext(navigationContext);
-  return !(userAccess >= (requiredAdmin ? 2 : 1)) ? (
-    <div className='nav-link disabled'>
-      <span className='material-symbols-outlined'>{icon}</span>
-      {isExpanded ? expandedText : ""}
-    </div>
-  ) : (
-    <Link className='nav-link' to={to} onClick={onClick}>
-      <span className='material-symbols-outlined'>{icon}</span>
-      {isExpanded ? expandedText : ""}
-    </Link>
-  );
+	requiredAdmin = requiredAdmin || false;
+	const { isExpanded, userAccess } = useContext(navigationContext);
+	return !(userAccess >= (requiredAdmin ? 2 : 1)) ? (
+		<div className='nav-link disabled'>
+			<span className='material-symbols-outlined'>{icon}</span>
+			<ExpandedText isExpanded={isExpanded} text={expandedText} />
+		</div>
+	) : (
+		<Link className='nav-link' to={to} onClick={onClick}>
+			<span className='material-symbols-outlined'>{icon}</span>
+			<ExpandedText isExpanded={isExpanded} text={expandedText} />
+		</Link>
+	);
 };
 
 export const SubBetterLink = ({ to, expandedText, icon, children, requiredAdmin }: SubBetterLinkProps) => {
-  requiredAdmin = requiredAdmin || false;
-  const { isExpanded, userAccess, location } = useContext(navigationContext);
-  return location.includes(to) ? (
-    <div className='nav-link-selected'>
-      {!(userAccess >= (requiredAdmin ? 2 : 1)) ? (
-        <div className='nav-link disabled'>
-          <span className='material-symbols-outlined'>{icon}</span>
-          {isExpanded ? expandedText : ""}
-        </div>
-      ) : (
-        <Link className='nav-header' to={to}>
-          <span className='material-symbols-outlined'>{icon}</span>
-          {isExpanded ? expandedText : ""}
-        </Link>
-      )}
-      {children}
-    </div>
-  ) : !(userAccess >= (requiredAdmin ? 2 : 1)) ? (
-    <div className='nav-link disabled'>
-      <span className='material-symbols-outlined'>{icon}</span>
-      {isExpanded ? expandedText : ""}
-    </div>
-  ) : (
-    <Link className='nav-link' to={to}>
-      <span className='material-symbols-outlined'>{icon}</span>
-      {isExpanded ? expandedText : ""}
-    </Link>
-  );
+	requiredAdmin = requiredAdmin || false;
+	const { isExpanded, userAccess, location } = useContext(navigationContext);
+	return location.includes(to) ? (
+		<div className='nav-link-selected'>
+			{!(userAccess >= (requiredAdmin ? 2 : 1)) ? (
+				<div className='nav-link disabled'>
+					<span className='material-symbols-outlined'>{icon}</span>
+					<ExpandedText isExpanded={isExpanded} text={expandedText} />
+				</div>
+			) : (
+				<Link className='nav-header' to={to}>
+					<span className='material-symbols-outlined'>{icon}</span>
+					<ExpandedText isExpanded={isExpanded} text={expandedText} />
+				</Link>
+			)}
+			{children}
+		</div>
+	) : !(userAccess >= (requiredAdmin ? 2 : 1)) ? (
+		<div className='nav-link disabled'>
+			<span className='material-symbols-outlined'>{icon}</span>
+			<ExpandedText isExpanded={isExpanded} text={expandedText} />
+		</div>
+	) : (
+		<Link className='nav-link' to={to}>
+			<span className='material-symbols-outlined'>{icon}</span>
+			<ExpandedText isExpanded={isExpanded} text={expandedText} />
+		</Link>
+	);
 };
--- MatAppClient/src/views/admin/navigation/index.tsx
+++ MatAppClient/src/views/admin/navigation/index.tsx
@@ -29,29 +29,33 @@ const Navigation = ({ userAccess }: NavigationProps) => {
 
 	return (
 		<navigationContext.Provider value={{ location: location.pathname, isExpanded: Expanded, userAccess }}>
-			<div className='nav'>
-				<div className='logo'>
-					<LogoSVG class='small' />
+			<div>
+				<div className='nav'>
+					<div className='nav-container'>
+						<div className='logo nav-leftAlign'>
+							<LogoSVG class='small' />
+						</div>
+						<div style={{ transform: Expanded ? "translateX(1em)" : "translateX(0em)" }}>
+							<BetterLink to='' expandedText='Home' icon='home' />
+							<BetterLink to='users' expandedText='Users' icon='group' />
+							<BetterLink to='ingredients' expandedText='Ingredients' icon='eco' />
+							<BetterLink to='dishes' expandedText='Dishes' icon='restaurant' />
+							<SubBetterLink to='branches' expandedText='Branches' icon='store'>
+								<BetterLink to='branches/reports' expandedText='Reports' icon='receipt_long' />
+								<BetterLink to='branches/storage' expandedText='Storage' icon='kitchen' />
+							</SubBetterLink>
+							<BetterLink to='information' requiredAdmin expandedText='Information' icon='info' />
+							<BetterLink to={location.pathname} expandedText='Refresh' icon='sync' onClick={refresh} />
+						</div>
+						<button className='nav-leftAlign' onClick={() => setExpanded(!Expanded)}>
+							{Expanded ? (
+								<span className='material-symbols-outlined'>west</span>
+							) : (
+								<span className='material-symbols-outlined'>east</span>
+							)}
+						</button>
+					</div>
 				</div>
-				<div>
-					<BetterLink to='' expandedText='Home' icon='home' />
-					<BetterLink to='users' expandedText='Users' icon='group' />
-					<BetterLink to='ingredients' expandedText='Ingredients' icon='eco' />
-					<BetterLink to='dishes' expandedText='Dishes' icon='restaurant' />
-					<SubBetterLink to='branches' expandedText='Branches' icon='store'>
-						<BetterLink to='branches/reports' expandedText='Reports' icon='receipt_long' />
-						<BetterLink to='branches/storage' expandedText='Storage' icon='kitchen' />
-					</SubBetterLink>
-					<BetterLink to='information' requiredAdmin expandedText='Information' icon='info' />
-					<BetterLink to={location.pathname} expandedText='Refresh' icon='sync' onClick={refresh} />
-				</div>
-				<button onClick={() => setExpanded(!Expanded)}>
-					{Expanded ? (
-						<span className='material-symbols-outlined'>west</span>
-					) : (
-						<span className='material-symbols-outlined'>east</span>
-					)}
-				</button>
 			</div>
 		</navigationContext.Provider>
 	);
