d8c5607aa11e8f98d80238afaf49602785e3ea87
ten-james <hexaplay2@gmail.com>
2023-02-02 09:25:53 +0100

    sql dumps

---
+++ sql/matapp-with-demo-data-202302020925.sql
@@ -0,0 +1,416 @@
+-- MySQL dump 10.13  Distrib 8.0.28, for macos11 (x86_64)
+--
+-- Host: localhost    Database: matapp
+-- ------------------------------------------------------
+-- Server version	8.0.28
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `branch$ingredients`
+--
+
+DROP TABLE IF EXISTS `branch$ingredients`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `branch$ingredients` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `branch_id` int NOT NULL,
+  `ingredient_id` int NOT NULL,
+  `count` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `ingredient_id` (`ingredient_id`),
+  KEY `branch$ingredients_ibfk_1` (`branch_id`),
+  CONSTRAINT `branch$ingredients_ibfk_1` FOREIGN KEY (`branch_id`) REFERENCES `branches` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `branch$ingredients_ibfk_2` FOREIGN KEY (`ingredient_id`) REFERENCES `ingredients` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `branch$ingredients`
+--
+
+LOCK TABLES `branch$ingredients` WRITE;
+/*!40000 ALTER TABLE `branch$ingredients` DISABLE KEYS */;
+INSERT INTO `branch$ingredients` VALUES (1,1,1,30),(2,1,2,5),(3,2,3,10),(4,2,4,7),(5,1,5,3),(6,2,6,4),(7,1,7,5),(8,2,8,10);
+/*!40000 ALTER TABLE `branch$ingredients` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `branches`
+--
+
+DROP TABLE IF EXISTS `branches`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `branches` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `location` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_czech_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `branches`
+--
+
+LOCK TABLES `branches` WRITE;
+/*!40000 ALTER TABLE `branches` DISABLE KEYS */;
+INSERT INTO `branches` VALUES (1,'Stánek','Varenská 30'),(2,'Pobočka','Marianskohorská 30');
+/*!40000 ALTER TABLE `branches` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `dish$ingredients`
+--
+
+DROP TABLE IF EXISTS `dish$ingredients`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `dish$ingredients` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `dish_id` int NOT NULL,
+  `ingredient_id` int NOT NULL,
+  `count` int NOT NULL,
+  `line` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `ingredient_id` (`ingredient_id`),
+  KEY `dish$ingredients_ibfk_1` (`dish_id`),
+  CONSTRAINT `dish$ingredients_ibfk_1` FOREIGN KEY (`dish_id`) REFERENCES `dishes` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_czech_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `dish$ingredients`
+--
+
+LOCK TABLES `dish$ingredients` WRITE;
+/*!40000 ALTER TABLE `dish$ingredients` DISABLE KEYS */;
+INSERT INTO `dish$ingredients` VALUES (6,2,2,1,0),(7,2,4,1,1),(8,2,1,1,1);
+/*!40000 ALTER TABLE `dish$ingredients` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `dish_categories`
+--
+
+DROP TABLE IF EXISTS `dish_categories`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `dish_categories` (
+  `id` int NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `dish_categories_UN` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_czech_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `dish_categories`
+--
+
+LOCK TABLES `dish_categories` WRITE;
+/*!40000 ALTER TABLE `dish_categories` DISABLE KEYS */;
+INSERT INTO `dish_categories` VALUES (0,'Hlavní chod');
+/*!40000 ALTER TABLE `dish_categories` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `dishes`
+--
+
+DROP TABLE IF EXISTS `dishes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `dishes` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `cost` int NOT NULL,
+  `category_id` int NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`),
+  KEY `dishes_ibfk_1` (`category_id`),
+  CONSTRAINT `dishes_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `dish_categories` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_czech_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `dishes`
+--
+
+LOCK TABLES `dishes` WRITE;
+/*!40000 ALTER TABLE `dishes` DISABLE KEYS */;
+INSERT INTO `dishes` VALUES (2,'jak',13,0),(3,'jak',13,0);
+/*!40000 ALTER TABLE `dishes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ingredient_allergens`
+--
+
+DROP TABLE IF EXISTS `ingredient_allergens`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ingredient_allergens` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `ingredient_id` int NOT NULL,
+  `num` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `ingredient_allergens_ibfk_1` (`ingredient_id`),
+  CONSTRAINT `ingredient_allergens_ibfk_1` FOREIGN KEY (`ingredient_id`) REFERENCES `ingredients` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_czech_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ingredient_allergens`
+--
+
+LOCK TABLES `ingredient_allergens` WRITE;
+/*!40000 ALTER TABLE `ingredient_allergens` DISABLE KEYS */;
+INSERT INTO `ingredient_allergens` VALUES (3,2,2),(4,2,3),(9,3,4),(16,1,41),(17,8,1),(18,8,21),(19,8,31);
+/*!40000 ALTER TABLE `ingredient_allergens` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ingredient_text_extensions`
+--
+
+DROP TABLE IF EXISTS `ingredient_text_extensions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ingredient_text_extensions` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `text` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_czech_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ingredient_text_extensions`
+--
+
+LOCK TABLES `ingredient_text_extensions` WRITE;
+/*!40000 ALTER TABLE `ingredient_text_extensions` DISABLE KEYS */;
+INSERT INTO `ingredient_text_extensions` VALUES (1,'ks'),(2,'g'),(3,'ml');
+/*!40000 ALTER TABLE `ingredient_text_extensions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ingredient_types`
+--
+
+DROP TABLE IF EXISTS `ingredient_types`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ingredient_types` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `ingredient_types_name_IDX` (`name`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_czech_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ingredient_types`
+--
+
+LOCK TABLES `ingredient_types` WRITE;
+/*!40000 ALTER TABLE `ingredient_types` DISABLE KEYS */;
+INSERT INTO `ingredient_types` VALUES (1,'-'),(3,'Maso'),(4,'Nápoje'),(22,'Pečivo'),(2,'Příloha'),(14,'Zelenina');
+/*!40000 ALTER TABLE `ingredient_types` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ingredients`
+--
+
+DROP TABLE IF EXISTS `ingredients`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ingredients` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `ingredient_type_id` int NOT NULL,
+  `cost` int NOT NULL DEFAULT '0',
+  `text` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `ingredient_text_extension_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `ingredients_FK` (`ingredient_text_extension_id`),
+  KEY `ingredients_ibfk_1` (`ingredient_type_id`),
+  CONSTRAINT `ingredients_FK` FOREIGN KEY (`ingredient_text_extension_id`) REFERENCES `ingredient_text_extensions` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `ingredients_ibfk_1` FOREIGN KEY (`ingredient_type_id`) REFERENCES `ingredient_types` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_czech_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ingredients`
+--
+
+LOCK TABLES `ingredients` WRITE;
+/*!40000 ALTER TABLE `ingredients` DISABLE KEYS */;
+INSERT INTO `ingredients` VALUES (1,'Chleba',22,10,'1',1),(2,'Párky',3,10,'2',1),(3,'Kečup',1,5,'50',3),(4,'Hořčice',1,5,'50',3),(5,'Rohlík',2,0,'2',1),(6,'Vajičko',1,7,'1',1),(7,'Slanina',3,10,'3',1),(8,'Tmavý Chleba',22,15,'1',3),(9,'Káva',4,10,'200',3),(10,'Džus',4,20,'250',3),(13,'Čistá voda',1,20,'1',1),(14,'Voda 2',1,20,'1',1);
+/*!40000 ALTER TABLE `ingredients` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `serve$dishes`
+--
+
+DROP TABLE IF EXISTS `serve$dishes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `serve$dishes` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `serve_id` int NOT NULL,
+  `dish_id` int NOT NULL,
+  `count` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `serve_id` (`serve_id`),
+  KEY `serve$dishes_ibfk_2` (`dish_id`),
+  CONSTRAINT `serve$dishes_ibfk_1` FOREIGN KEY (`serve_id`) REFERENCES `serves` (`id`),
+  CONSTRAINT `serve$dishes_ibfk_2` FOREIGN KEY (`dish_id`) REFERENCES `dishes` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_czech_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `serve$dishes`
+--
+
+LOCK TABLES `serve$dishes` WRITE;
+/*!40000 ALTER TABLE `serve$dishes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `serve$dishes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `serve_sessions`
+--
+
+DROP TABLE IF EXISTS `serve_sessions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `serve_sessions` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `branch_id` int NOT NULL,
+  `s_date` date NOT NULL,
+  `e_date` date NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `serve_sessions_ibfk_1` (`branch_id`),
+  CONSTRAINT `serve_sessions_ibfk_1` FOREIGN KEY (`branch_id`) REFERENCES `branches` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_czech_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `serve_sessions`
+--
+
+LOCK TABLES `serve_sessions` WRITE;
+/*!40000 ALTER TABLE `serve_sessions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `serve_sessions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `serve_type`
+--
+
+DROP TABLE IF EXISTS `serve_type`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `serve_type` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_czech_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `serve_type`
+--
+
+LOCK TABLES `serve_type` WRITE;
+/*!40000 ALTER TABLE `serve_type` DISABLE KEYS */;
+/*!40000 ALTER TABLE `serve_type` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `serves`
+--
+
+DROP TABLE IF EXISTS `serves`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `serves` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `serve_date` date NOT NULL,
+  `session_id` int NOT NULL,
+  `type_id` int NOT NULL,
+  `cost` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `type_id` (`type_id`),
+  KEY `serves_ibfk_1` (`session_id`),
+  CONSTRAINT `serves_ibfk_1` FOREIGN KEY (`session_id`) REFERENCES `serve_sessions` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `serves_ibfk_2` FOREIGN KEY (`type_id`) REFERENCES `serve_type` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_czech_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `serves`
+--
+
+LOCK TABLES `serves` WRITE;
+/*!40000 ALTER TABLE `serves` DISABLE KEYS */;
+/*!40000 ALTER TABLE `serves` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `branch_id` int DEFAULT NULL,
+  `access` int NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`),
+  KEY `users_ibfk_1` (`branch_id`),
+  CONSTRAINT `users_ibfk_1` FOREIGN KEY (`branch_id`) REFERENCES `branches` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_czech_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'admin','21232f297a57a5a743894a0e4a801fc3',NULL,2),(2,'pob','098f6bcd4621d373cade4e832627b4f6',1,1);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'matapp'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-02-02  9:25:45
