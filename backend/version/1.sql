df33449c4d899af73568ebf492d30410bd46a16e
Ten-James <hexaplay2@gmail.com>
2023-03-12 15:08:25 +0100

    sql fix2

--- backend/version/1.sql
+++ backend/version/1.sql
@@ -85,7 +85,7 @@ CREATE TABLE
         KEY `branch$ingredients_ibfk_1` (`branch_id`),
         CONSTRAINT `branch$ingredients_ibfk_1` FOREIGN KEY (`branch_id`) REFERENCES `branches` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
         CONSTRAINT `branch$ingredients_ibfk_2` FOREIGN KEY (`ingredient_id`) REFERENCES `ingredients` (`id`)
-    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_czech_ci;
 CREATE TABLE
     `serve_type` (
         `id` int NOT NULL AUTO_INCREMENT,
